b4441d2e9bc04f34e04caa488eca22c99e530baf
wl39 <iwg6852@gmail.com>
2025-06-28 18:44:15 +0900

    Error fixed

--- src/components/UserPageCard/UserPageCard.jsx
+++ src/components/UserPageCard/UserPageCard.jsx
@@ -1,17 +1,17 @@
-import { Suspense, use, useEffect } from 'react';
+import { Suspense, useEffect } from 'react';
 import Card from '../Card/Card';
 
 const UserPageCard = ({ username }) => {
-  const user = use(getAssignmentCounts(username));
+  //   const user = use(getAssignmentCounts(username));
   useEffect(() => {
     const getAssignmentCounts = async () => {
-      const data = await getAssignmentCounts(user.username);
+      //   const data = await getAssignmentCounts(user.username);
     };
   }, []);
   return (
     <Card>
-      <div>{user.username}</div>
-      <div>{user.email}</div>
+      {/* <div>{user.username}</div>
+      <div>{user.email}</div> */}
     </Card>
   );
 };
